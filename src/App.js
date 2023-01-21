099fb13cbc0e0a7602b2d2853f1901b14b42f3b0
SHUNNO-ZERO <mahmudasif616@gmail.com>
2023-01-21 17:16:07 +0600

    for now slidebar component  added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 import Navbar from './Pages/Shared/Navbar/Navbar';
 import Footer from './Pages/Shared/Footer/Footer';
+import Slide from './Pages/Shared/Slide/Slide';
+ 
+ 
+
+ 
+
+
+/*****    TRYING TO USE COMMENT AND FEEDBACK                 ******** */
+
+ // apatoto component gula emni dekhar jonno edike rakchhi ... 
 
 function App() {
   return (
     <div className="App">
+      <>
+           
 
       <Navbar/>
+      <Slide/>
       <Footer/>
+      
+      
+      
+
+
+      </>
+     
+     
+      
       
     </div>
   );
